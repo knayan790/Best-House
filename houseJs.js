d322fac7d34e2f7fcabc4a14f3f13fbea907554e
nayanKaley <knayan790@gmail.com>
2022-01-08 16:07:43 +0530

    saving in github

--- houseJs.js
+++ houseJs.js
@@ -3,3 +3,15 @@ let navbar=document.getElementById("navBar");
 function togglebtn(){
     navbar.classList.toggle("hideMenu");
 }
+function UserLogin(){
+    let findYour=document.getElementsByClassName("find-your")[0];
+    findYour.style.display="none";
+    let userLogin=document.getElementsByClassName("user-login")[0];
+    userLogin.style.display="block";
+}
+function cancelBtn(){
+    let userLogin=document.getElementsByClassName("user-login")[0];
+    userLogin.style.display="none";
+    let findYour=document.getElementsByClassName("find-your")[0];
+    findYour.style.display="block";
+}
