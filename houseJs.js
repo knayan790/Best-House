5181dcf49f2bc85340a870237912bdd82897a6bd
knayan790 <77727286+knayan790@users.noreply.github.com>
2022-01-08 16:35:02 +0530

    Add files via upload
    js file

--- houseJs.js
+++ houseJs.js
@@ -3,7 +3,6 @@ let navbar=document.getElementById("navBar");
 function togglebtn(){
     navbar.classList.toggle("hideMenu");
 }
-<<<<<<< HEAD
 function UserLogin(){
     let findYour=document.getElementsByClassName("find-your")[0];
     findYour.style.display="none";
@@ -16,14 +15,3 @@ function cancelBtn(){
     let findYour=document.getElementsByClassName("find-your")[0];
     findYour.style.display="block";
 }
-=======
-let minimize=document.getElementById("mini");
-var loginBox=document.getElementById("miss");
-minimize.addEventListener("click", function(){
-    loginBox.style.display="none";
-});
-let registerBtn=document.getElementById("trigger");
-registerBtn.addEventListener("click", function(){
-    loginBox.style.display="block";
-});
->>>>>>> 7c16f7921ec93bd623bd4d65108a43a5d857c9a3
